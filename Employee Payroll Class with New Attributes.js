90f889df8b9c7932c306269ba44dbae235183d0d
Prachi1808 <prachimohanty.1808@gmail.com>
2022-05-03 16:25:54 +0530

    Add EmployeePayrollData

---
+++ Employee Payroll Class with New Attributes.js
@@ -0,0 +1,49 @@
+class EmployeePayrolData {
+    /**
+     * getter and stter method
+     */
+    get id() { return this._id; }
+    set id(id) {
+        this._id = id;
+    }
+    get name() { return this._name; }
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{a-zA-Z\\s]{2,}$')
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect!';
+    }
+    get profilePic() { return this._profilePic; }
+    set profilePic(prfilePic) {
+        this._profilePic = profilePic;
+    }
+    get gender() { return this._gender; }
+    set gender(gender) {
+        this._gender = gender;
+    }
+    get deparment() { return this._department; }
+    set department(department) {
+        this._department = department;
+    }
+    get salary() { return this._salary; }
+    set salary(salary) {
+        this._salary = salary;
+    }
+    get note() { return this._note; }
+    set note(note) {
+        this._note = note;
+    }
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+    /**
+     * method
+     */
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "underfined" :
+            this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender + ", profilePic='" + this.profilePic + ",deparment=" + this.department + ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+    }
+}
